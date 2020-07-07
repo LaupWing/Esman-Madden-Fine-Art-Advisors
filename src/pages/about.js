ea1588b5b1c21536dfdc996b52395abc741769a5
LaupWing <loc_strict@hotmail.com>
2020-07-07 14:40:46 +0200

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+
 const AboutPage = () => {
     return(
-        <div>
-
-        </div>
+        <Layout>
+            <div>
+                <h1>About us</h1>
+            </div>
+        </Layout>
     );
 }
 
