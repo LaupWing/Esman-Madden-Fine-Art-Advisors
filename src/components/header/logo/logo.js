e600a8b4edda51c2a2ae3acfcb707390b4ad18ce
LaupWing <loc_strict@hotmail.com>
2020-07-09 14:22:17 +0200

    base styling

---
+++ src/components/header/logo/logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "gatsby"
+import styles from './logo.module.css'
+
+export default function menu() {
+
+    return (
+        <h1 className={styles.logo}>
+            <Link to="/">
+
+            </Link>
+        </h1>
+    )
+}
