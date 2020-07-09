94db210850fc8d073adf12fc6c2e1a848ee10e2b
LaupWing <loc_strict@hotmail.com>
2020-07-09 14:32:14 +0200

    updated

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,32 +2,16 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Nav from './nav/nav'
+import Logo from './logo/logo'
 import styles from "./header.module.css"
+
 
 const Header = ({ siteTitle }) => (
     <header
         className={styles.header}
     >
-        <div
-        style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
-        }}
-        >
-        <h1 style={{ margin: 0 }}>
-            <Link
-                to="/"
-                style={{
-                    color: `white`,
-                    textDecoration: `none`,
-                }}
-            >
-                {siteTitle}
-            </Link>
-        </h1>
+        <Logo/>
         <Nav/>
-        </div>
     </header>
 )
 
--- src/components/header/logo/logo.js
+++ src/components/header/logo/logo.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from "gatsby"
 import styles from './logo.module.css'
 
-export default function menu() {
-
+export default function logo() {
     return (
         <h1 className={styles.logo}>
             <Link to="/">
-
+                <span>esman Madden</span>
+                <span>Fine Art</span>
+                <span>Advisors</span>
             </Link>
         </h1>
     )
